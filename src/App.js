0c9ac41601dc4cb18d7f816872f5a0668f6a62c4
Lolotte Schimmel <lmcschimmel@gmail.com>
2021-02-23 14:01:55 +0100

    get to do function return list

--- src/App.js
+++ src/App.js
@@ -5,19 +5,29 @@ class App extends Component {
 
   state = {
     title: '',
-    description: ''
+    description: '',
+    toDos: [ ],
   }
 
   createTodo = () => {
-      axios.post('http://localhost:8080/api/v1/todos', this.state)
+      axios.post('http://localhost:8080/api/v1/todos', {title: this.state.title, description: this.state.description}).then(
+        (response) => {
+        this.getTodo()
+        }
+      )
   }
 
   getTodo = () => {
     axios.get('http://localhost:8080/api/v1/todos').then(
       (response) => {
         console.log(response.data)
+        this.setState({toDos: response.data})
       }
     )
+  }
+
+  componentDidMount = () => {
+    this.getTodo();
   }
 
   render() {   
@@ -35,7 +45,8 @@ class App extends Component {
         </button>
         <div className="column">
           <ul>
-          {this.getTodo()}
+            {this.state.toDos.map((toDos, index) => (
+              <li key={index}>{toDos.title}: {toDos.description}</li> ))}
           </ul>
         </div>  
       </div>              
