ec89c513ee6707c491c058adcf5246173cd29b9f
Lolotte Schimmel <lmcschimmel@gmail.com>
2021-03-01 16:15:07 +0100

    toggle description function

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,7 +2,10 @@ import React, {Component} from 'react'
 import axios from 'axios';
 
 class TodoItem extends Component {
- 
+  
+  state = {
+    showDetails: false,
+  }
 
   deleteTodo = (id) => {
     axios.delete('http://localhost:8080/api/v1/todos/' + id).then(
@@ -12,11 +15,22 @@ class TodoItem extends Component {
     )
   }
 
+  toggleDescription = () => {
+    this.setState((prevState) => ({
+      showDetails: !prevState.showDetails,
+    }))
+  }
+
 
 render() {
   return ( 
     <li>
-      {this.props.value.title}: {this.props.value.description}
+      <div onClick={this.toggleDescription}>
+        {this.props.value.title}
+      </div>
+      <div hidden = {!this.state.showDetails}>
+        {this.props.value.description}
+      </div>
       <button onClick={() => {this.deleteTodo(this.props.value.id)}}>
         Delete this to do!
       </button>
